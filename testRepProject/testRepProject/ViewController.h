b1f2a462fb1045e9ce01db69251522776639785b
Manjunadh Bommisetty <manjunadhb@manjunadhs-macbook-pro.local>
2016-08-28 21:16:18 +0530

    Initial Version of my Project

---
+++ testRepProject/testRepProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testRepProject
+//
+//  Created by Manjunadh Bommisetty on 28/08/16.
+//  Copyright © 2016 BRN Infotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testRepProject/testRepProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testRepProject
+//
+//  Created by Manjunadh Bommisetty on 28/08/16.
+//  Copyright © 2016 BRN Infotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
